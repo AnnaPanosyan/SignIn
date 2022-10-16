83aee41c82f07ed406cc17aeec3158096b376264
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2022-10-16 23:40:00 +0400

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7Pn1zTklKJLW71PSL_quFG2OI5jMwxi4",
-  authDomain: "fir-auth-article-66da6.firebaseapp.com",
-  projectId: "fir-auth-article-66da6",
-  storageBucket: "fir-auth-article-66da6.appspot.com",
-  messagingSenderId: "419126010838",
-  appId: "1:419126010838:web:8936b8a9643f3eb44f7a77",
-  measurementId: "G-7F0CNKBNE5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 
 
 };
